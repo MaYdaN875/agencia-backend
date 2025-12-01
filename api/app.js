91685d8153b0fc5873ce9641401ecd7474d294f5
angel ca <danielleon0100@gmail.com>
2025-11-30 23:58:40 -0600

    Conexion sql agregada, MVC AGREgado, primer CRUD con RESTFUL flights creado

--- api/app.js
+++ api/app.js
@@ -1,25 +1,19 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import mysql from "mysql2/promise";
-
+import { connection } from "./database/config.js";
+import flightRoutes from './routes/flightRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-const db = await mysql.createConnection({
-    host: process.env.DB_HOST || "db",
-    user: process.env.DB_USER || "appuser",
-    password: process.env.DB_PASSWORD || "apppassword",
-    database: process.env.DB_NAME || "mi_app",
-});
-
-app.get("/api/test", async (req, res) => {
-    const [rows] = await db.query("SELECT 'API funcionando con Docker y MySQL' AS mensaje");
-    res.json(rows[0]);
-});
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API escuchando en puerto ${PORT}`));
+
+connection();
+
+app.use(express.json());
+
+// Rutas
+app.use('/api/flights', flightRoutes);
+
+app.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
+
+export default app;
